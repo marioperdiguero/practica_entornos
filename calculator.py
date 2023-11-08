2ec92563d27c9c86dc079eb20edb66dbd219f016
Mario <marioperdiguero37@gmail.com>
2023-11-08 11:03:08 +0100

    📝 Agregar documentación a las funciones

--- calculator.py
+++ calculator.py
@@ -1,16 +1,31 @@
 def add(a, b):
+    """"
+        Función que retorna la suma de dos números
+    """
     return a + b
 
 def subtract(a, b):
+    """"
+        Función que retorna la resta de dos números
+    """
     return a - b
 
 def multiply(a, b):
+    """"
+        Función que retorna la multiplicación de dos números
+    """
     pass
 
 def divide(a, b):
+    """"
+        Función que retorna la división de dos números
+    """
     return a / b
 
 def menu_choice():
+    """"
+        Función que muestra el menú de la aplicación
+    """
     funciones = ["sumar", "restar", "multiplicar", "dividir"]
 
     for i in range(len(funciones)):
